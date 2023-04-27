8d0b917f25c5219ce1ea31259565c0574faaf471
ZulqarnainRashid <zulqarnain_786@hotmail.com>
2023-04-27 22:08:06 +0100

    score plays sound

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -9,12 +9,20 @@ public class LogicScript : MonoBehaviour
     public int playerScore;
     public Text scoreText;
     public GameObject gameOverScreen;
+    public AudioSource scoreSFX;
+
+
+    void Start()
+    {
+        scoreSFX = GetComponent<AudioSource>();
+    }
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
     {
         playerScore += scoreToAdd;
         scoreText.text = playerScore.ToString();
+        scoreSFX.Play();
     }
 
     public void restartGame()
